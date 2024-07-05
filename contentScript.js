fd489fb967f1f1c9156d02e89f1b2460a9f72e3b
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-07-05 16:43:02 -0500

    reindent

--- contentScript.js
+++ contentScript.js
@@ -10,51 +10,51 @@ function getMetaContentByName(name) {
 }
 
 function insertAfter(referenceNode, newNode) {
-	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 let repoName = getMetaContentByName('octolytics-dimension-repository_nwo');
 let apiUrl = 'https://api.github.com/repos/' + repoName + '/releases/latest';
 
 fetch(apiUrl, {
-	headers: {
-		'Accept': 'application/vnd.github+json',
-		'X-GitHub-Api-Version': '2022-11-28'
-	}
+  headers: {
+    'Accept': 'application/vnd.github+json',
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
 })
-	.then(response => {
-		if (!response.ok) {
-			throw new Error('Network response was not ok');
-		}
-		renderButton();
-	})
-	.catch(_ => {
-	});
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    renderButton();
+  })
+  .catch(_ => {
+  });
 
 function renderButton() {
-	let url = getMetaContentByProperty('og:url') + '/releases/latest';
-	// creating the button
-	const container = document.createElement('div');
-	container.style.display = 'flex';
-	container.style.justifyContent = 'center';
-	container.style.alignItems = 'center';
+  let url = getMetaContentByProperty('og:url') + '/releases/latest';
+  // creating the button
+  const container = document.createElement('div');
+  container.style.display = 'flex';
+  container.style.justifyContent = 'center';
+  container.style.alignItems = 'center';
 
-	const button = document.createElement('a');
+  const button = document.createElement('a');
 
-	button.innerText = 'Latest Release';
+  button.innerText = 'Latest Release';
 
-	button.style.padding = '20px 40px';
-	button.style.border = '1px outset buttonborder';
-	button.style.borderRadius = '3px';
-	button.style.color = '#ffffff';
-	button.style.backgroundColor = '#238636';
-	button.style.textDecoration = 'none';
-	button.style.marginTop = '10px';
-	button.style.width = '65%';
-	button.style.textAlign = 'center';
-	button.href = url;
+  button.style.padding = '20px 40px';
+  button.style.border = '1px outset buttonborder';
+  button.style.borderRadius = '3px';
+  button.style.color = '#ffffff';
+  button.style.backgroundColor = '#238636';
+  button.style.textDecoration = 'none';
+  button.style.marginTop = '10px';
+  button.style.width = '65%';
+  button.style.textAlign = 'center';
+  button.href = url;
 
-	container.appendChild(button)
-	var header = document.getElementById('repository-container-header');
-	insertAfter(header, container)
+  container.appendChild(button)
+  var header = document.getElementById('repository-container-header');
+  insertAfter(header, container)
 };
